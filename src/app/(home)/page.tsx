cad2e152c3120fba7d8eab5bda576943fc95cbea
lucas <lucasrenan365@outlook.com>
2023-11-08 19:44:56 -0300

    [feat] - add product list keyboard and mouses

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import { Flex, HomeContainer, Text } from "./styles";
 import banner50off from "@/public/banner-55off.png";
 import banner50offmouses from "@/public/banner-mouses.png";
+import bannerFones from "@/public/banner-fones.png";
 
 import Categories from "./components/Categories";
 import { prismaClient } from "../../lib/prisma";
@@ -12,6 +13,22 @@ export default async function Home() {
     where: {
       discountPercentage: {
         gt: 0,
+      },
+    },
+  });
+
+  const keyboards = await prismaClient.product.findMany({
+    where: {
+      category: {
+        slug: "keyboards",
+      },
+    },
+  });
+
+  const mouses = await prismaClient.product.findMany({
+    where: {
+      category: {
+        slug: "mouses",
       },
     },
   });
@@ -40,6 +57,24 @@ export default async function Home() {
         width={0}
         className="banner"
       />
+
+      <Flex direction={"column"} mt={"lg"}>
+        <Text>Teclados</Text>
+        <ProductList products={keyboards} />
+      </Flex>
+
+      <Image
+        src={bannerFones}
+        alt="banner 55% off em mouses"
+        height={0}
+        width={0}
+        className="banner"
+      />
+
+      <Flex direction={"column"} mt={"lg"}>
+        <Text>Mouses</Text>
+        <ProductList products={mouses} />
+      </Flex>
     </HomeContainer>
   );
 }
