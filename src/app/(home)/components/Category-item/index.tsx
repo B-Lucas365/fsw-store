1a52751fa2c83224fe958a9f89a6e86566cba378
lucas <lucasrenan365@outlook.com>
2023-11-10 17:53:47 -0300

    [feat] - add link in category item home page

--- src/app/(home)/components/Category-item/index.tsx
+++ src/app/(home)/components/Category-item/index.tsx
@@ -2,6 +2,7 @@ import { Category } from "@prisma/client";
 import { Flex } from "./styles";
 import { Text } from "@mantine/core";
 import { CATERGORY_ICON } from "../../../../constants/category-icon";
+import Link from "next/link";
 
 interface ItemProps {
   category: Category;
@@ -9,11 +10,13 @@ interface ItemProps {
 
 export const CategoryItem = ({ category }: ItemProps) => {
   return (
-    <Flex color="#fff">
+    <Link href={`/category/${category.slug}`}>
+      <Flex color="#fff">
       {CATERGORY_ICON[category.slug as keyof typeof CATERGORY_ICON]}
       <Text span={true} size="sm">
         {category.name}
       </Text>
     </Flex>
+    </Link>
   );
 };
